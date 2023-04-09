3dc1fbce5466dd04a518849354e3332318222afb
kazimahin68 <kazimahin68@gmail.com>
2023-04-10 03:53:12 +0600

    API Data created

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h2>This Is home page</h2>
+        </div>
+    );
+};
+
+export default Home;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import App from './App';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import './index.css';
+import Home from './components/Home/Home';
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home></Home>
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router = {router}></RouterProvider>
   </React.StrictMode>,
 )
